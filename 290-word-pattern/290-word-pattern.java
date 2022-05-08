167f58b24845449eefe28b78d684a690a3d28751
Mohamed Hossam <33866290+mohamedhossam822@users.noreply.github.com>
2022-05-08 06:06:34 +0200

    Time: 1 ms (88.26%), Space: 42.6 MB (5.96%) - LeetHub

--- 290-word-pattern/290-word-pattern.java
+++ 290-word-pattern/290-word-pattern.java
@@ -2,25 +2,20 @@ import java.util.Hashtable;
 class Solution {
     public boolean wordPattern(String pattern, String s) {
         Hashtable<Character,String> hs=new Hashtable<>();
-        Hashtable<String,Character> hs2=new Hashtable<>();
         String[] arrOfStr = s.split(" ");
         if(arrOfStr.length!=pattern.length()) return false;
         int n=arrOfStr.length;
         char ch;
         for(int i=0;i<n;i++){
             ch=pattern.charAt(i);
-            if(!hs.containsKey(ch) && !hs2.containsKey(arrOfStr[i])){
+            if(!hs.containsKey(ch)){
+                if(hs.containsValue(arrOfStr[i])) return false;
                 hs.put(ch,arrOfStr[i]);
-                hs2.put(arrOfStr[i],ch);
             }
             else{
-                if(!hs.containsKey(ch)) return false;
-                if(!hs2.containsKey(arrOfStr[i])) return false;
-                if(!hs.get(ch).equals(arrOfStr[i]) || !hs2.get(arrOfStr[i]).equals(ch)) 
+                if(!hs.get(ch).equals(arrOfStr[i])) 
                     return false;
             }
-            System.out.println(hs.entrySet());
-            System.out.println(hs2.entrySet());
         }
         return true;
     }
