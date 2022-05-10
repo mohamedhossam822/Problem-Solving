419ed528ba218744a71910374aaecebb911f4c6f
Mohamed Hossam <33866290+mohamedhossam822@users.noreply.github.com>
2022-05-10 07:18:49 +0200

    Time: 36 ms (46.06%), Space: 64.1 MB (34.66%) - LeetHub

--- 187-repeated-dna-sequences/187-repeated-dna-sequences.java
+++ 187-repeated-dna-sequences/187-repeated-dna-sequences.java
@@ -1,24 +1,22 @@
-import java.util.Hashtable;
+import java.util.HashSet;
 class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
-        List<String> result= new ArrayList<>();
-        Hashtable<String, Integer> strs=new Hashtable<>();
+        HashSet<String> result= new HashSet<>();
+        HashSet<String> strs=new HashSet<>();
         int n=s.length();
         int l=0;
         int r=9;
         String tempS;
         while(r<n){
             tempS=s.substring(l, r+1);
-            if(strs.containsKey(tempS)){
-                if(strs.get(tempS)!=-1){
-                    result.add(tempS);
-                    strs.put(tempS,-1);
-                }
+            if(strs.contains(tempS)){
+                result.add(tempS);
+                strs.add(tempS);
             }
-            else strs.put(tempS,1);
+            else strs.add(tempS);
             r++;
             l++;
         }
-        return result;
+        return new ArrayList(result);
     }
 }
