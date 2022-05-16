c226bbe6737dabbdb5cea927adeb84bc289a11bd
Mohamed Hossam <33866290+mohamedhossam822@users.noreply.github.com>
2022-05-16 20:28:38 +0200

    Time: 259 ms (5.76%), Space: 40.5 MB (61.37%) - LeetHub

---
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+class Solution {
+    //HashMap<Integer,Integer> cache;
+    public int numTrees(int n) {
+        //cache=new HashMap<>();
+        return num(n);
+    }
+    private int num(int n){
+        //if(cache.containsKey(n)) return cache.get(n);
+        if(n==0 || n==1) return 1;
+        if(n==2) return 2;
+        if(n==3) return 5;
+        if(n==4) return 14;
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            sum=sum+numTrees(i-1)*numTrees(n-i);
+        }
+        //cache.put(n,sum);
+        return sum;
+    }
+}
