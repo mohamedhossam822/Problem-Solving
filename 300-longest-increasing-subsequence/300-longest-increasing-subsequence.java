956eaa8d303503b7484b5b9f48ecb8c0cb86893c
Mohamed Hossam <33866290+mohamedhossam822@users.noreply.github.com>
2022-05-19 00:17:12 +0200

    Time: 189 ms (6.91%), Space: 44.8 MB (23.24%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,32 +1,26 @@
 class Solution {
     int[] grid;
     int n;
-    int globalSum=0;
+    int globalSum;
     public int lengthOfLIS(int[] nums) {
         n=nums.length;
         grid=new int[n];
-        if(n==1) return 1;
-        int val=getMax(0,nums);
+        globalSum=1;
         getMax(0,nums);
         return globalSum;
     }
     private int getMax(int i,int[] nums){
         if(grid[i]!=0) return grid[i];
-        int otherSum=0;
         int sum=0;
         int maxSum=1;
         for(int r=i+1;r<n;r++){
             sum=getMax(r,nums);
-            if(nums[i]<nums[r]){
-                if(sum+1>maxSum) maxSum=sum+1;
-                if(sum+1>globalSum) globalSum=sum+1;
-                
+            if(nums[i]<nums[r] && sum+1>maxSum){
+                maxSum=sum+1;
             } 
-            else{
-                if(sum>globalSum) globalSum=sum;
-            }
         }
         grid[i]=maxSum;
+        if(maxSum>globalSum) globalSum=maxSum;
         return maxSum;
     }
 }
