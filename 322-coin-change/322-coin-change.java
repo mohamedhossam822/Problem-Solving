b6b6f5e19ad805f4348e4bd911f72a6b9d4d8be0
Mohamed Hossam <33866290+mohamedhossam822@users.noreply.github.com>
2022-05-20 16:33:02 +0200

    Time: 467 ms (5.02%), Space: 145 MB (5.14%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,23 +1,22 @@
-import java.util.HashMap;
 class Solution {
-    HashMap<Integer, Integer> dp;
     public int coinChange(int[] coins, int amount) {
-        dp = new HashMap<>();
-        int val=getMin(coins,amount);
-        if(val==Integer.MAX_VALUE) return -1;
-        return val;
-    }
-    private int getMin(int[] coins, int amount){
-        if (amount==0) return 0;
-        if (amount<0) return Integer.MAX_VALUE;
-        if(dp.containsKey(amount)) return dp.get(amount);
-        int min=Integer.MAX_VALUE;
-        int val=min;
-        for(int i=0;i<coins.length;i++){
-            val=getMin(coins,amount-coins[i]);
-            if(val<min) min=val+1;
+        HashMap<Integer, Integer> dp = new HashMap<>();
+        dp.put(0,0);
+        int temp;
+        for(int i=1;i<=amount;i++){
+            for(int j=0;j<coins.length;j++){
+                if(i==coins[j]) dp.put(i,1);
+                else if(i>coins[j]){
+                    if(dp.containsKey(i-coins[j])){
+                        temp=1+dp.get(i-coins[j]);
+                        if(dp.containsKey(i)){
+                            if(temp<dp.get(i)) dp.put(i,temp);
+                        }else dp.put(i,temp);
+                    }
+                }
+            }
         }
-        dp.put(amount,min);
-        return min;
+        if(dp.containsKey(amount)) return dp.get(amount);
+        return -1;
     }
 }
