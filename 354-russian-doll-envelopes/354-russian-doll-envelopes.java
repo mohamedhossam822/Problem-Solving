6b84dab63a28a45b7dbf7e3157e1d8887520f36e
Mohamed Hossam <33866290+mohamedhossam822@users.noreply.github.com>
2022-05-26 01:22:35 +0200

    Time: 63 ms (68.55%), Space: 84.8 MB (37.29%) - LeetHub

---
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+
+class Solution {
+
+    public int maxEnvelopes(int[][] envelopes) {
+        Arrays.sort(envelopes, (a, b) -> a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]);
+
+        int[] dp = new int[envelopes.length];
+        int maxlen = 0;
+
+        for (int i = 0; i < dp.length; i++) {
+            int index = binarySearch(dp, 0, maxlen, envelopes[i][1]);
+            dp[index] = envelopes[i][1];
+
+            if (index == maxlen) {
+                maxlen++;
+            }
+        }
+        return maxlen;
+    }
+
+    int binarySearch(int[] arr, int start, int end, int target) {
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] > target) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+}
