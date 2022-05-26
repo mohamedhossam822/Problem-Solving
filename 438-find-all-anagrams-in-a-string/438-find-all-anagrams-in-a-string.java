3f82850fac312899569191272c37cfb6c610b39d
Mohamed Hossam <33866290+mohamedhossam822@users.noreply.github.com>
2022-05-27 01:58:26 +0200

    Time: 13 ms (64.65%), Space: 47.7 MB (53.04%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
@@ -11,10 +11,8 @@ class Solution {
         for(i=0;i<s.length();i++){
             if(i<n || s.charAt(i)!=s.charAt(i-n)){
                 f=0;
-                if(i-n>=0){
-                    num[s.charAt(i)-97]--;
-                    num[s.charAt(i-n)-97]++;
-                } else num[s.charAt(i)-97]--;
+                num[s.charAt(i)-97]--;
+                if(i>=n) num[s.charAt(i-n)-97]++; 
             }
             if(f==1) continue;
             for(int j=0;j<26;j++){
@@ -25,9 +23,7 @@ class Solution {
             }
             if(f==0) res.add(i-n+1);
         }
-        
-        
-        
+    
         return res;
     }
 }
