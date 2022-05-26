499add06c76df728c4ca283bd63e8b5aecd1d7c5
Mohamed Hossam <33866290+mohamedhossam822@users.noreply.github.com>
2022-05-27 01:46:52 +0200

    Time: 9 ms (81.99%), Space: 47 MB (68.08%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
@@ -1,44 +1,28 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        char c;
-        int n,i,f,nP=p.length();
+        int i,f,n=p.length();
         List<Integer> res=new  ArrayList<>();
-        HashSet<Character> pCharSet=new HashSet<>();
-        char[] pChar;
-        HashMap<Character,Integer> pFreq=new HashMap<>();
-        HashMap<Character,Integer> sFreq=new HashMap<>();
-        for(i=0;i<p.length();i++){
-            c=p.charAt(i);
-            pCharSet.add(c);
-            if(pFreq.containsKey(c)) pFreq.put(c,pFreq.get(c)+1);
-            else pFreq.put(c,1);
-        }
+        int[] numP=new int[26];
+        int[] numS=new int[26];
         
-        n=pCharSet.size();
-        pChar=new char[n];
-        i=0;
-        f=0;
-        for (char ch : pCharSet) {
-            pChar[i]=ch;
-            i++;
+        for(i=0;i<p.length();i++){
+            numP[p.charAt(i)-97]++;
         }
+        f=0;
         for(i=0;i<s.length();i++){
-            if(i<nP || s.charAt(i)!=s.charAt(i-nP)){
+            if(i<n || s.charAt(i)!=s.charAt(i-n)){
                 f=0;
-                c=s.charAt(i);
-                if(sFreq.containsKey(c)) sFreq.put(c,sFreq.get(c)+1);
-                else sFreq.put(c,1);
-                if(i-nP>=0)  sFreq.put(s.charAt(i-nP),sFreq.get(s.charAt(i-nP))-1);
+                numS[s.charAt(i)-97]++;
+                if(i-n>=0)  numS[s.charAt(i-n)-97]--;
             }
             if(f==1) continue;
-            for(int j=0;j<n;j++){
-                c=pChar[j];
-                if(!sFreq.containsKey(c) || sFreq.get(c).intValue()!=pFreq.get(c).intValue()){
+            for(int j=0;j<26;j++){
+                if(numS[j]!=numP[j]){
                     f=1;
-                    break;
-                }
+                    break; 
+                }   
             }
-            if(f==0) res.add(i-nP+1);
+            if(f==0) res.add(i-n+1);
         }
         
         
