4fc8cfaa0b545142fd23ced5ef2a6ce883203f2b
Mohamed Hossam <33866290+mohamedhossam822@users.noreply.github.com>
2022-05-28 07:34:19 +0200

    Time: 10 ms (77.55%), Space: 55.9 MB (48.49%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -2,44 +2,25 @@ import java.util.Hashtable;
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         List<List<String>> result=new ArrayList<>();
-        int n=strs.length;
-        Str[] sortedStrs=new Str[n];
+        List<String> temp;
         char[] tempArray;
-        //Sort all strings
-        for(int i=0;i<n;i++){
+        String s;
+        int counter=0;
+        HashMap<String,Integer> keyIndex=new HashMap<>();
+        
+        for(int i=0;i<strs.length;i++){
             tempArray = strs[i].toCharArray();
             Arrays.sort(tempArray);
-            sortedStrs[i]=new Str(new String(tempArray),i);
-        }
-        //Sort th resultant array of strings
-         Arrays.sort(sortedStrs, new Comparator<Str>() {
-           public int compare(Str b1, Str b2) {
-               return (b1.s).compareTo(b2.s);
-           }
-        });
-        List<String> newEntry=new ArrayList<>();
-        newEntry.add(strs[sortedStrs[0].index]);
-        for(int i=1;i<n;i++){
-            if(sortedStrs[i].s.equals(sortedStrs[i-1].s)){
-                newEntry.add(strs[sortedStrs[i].index]);
-            }
+            s=String.valueOf(tempArray);
+            if(keyIndex.containsKey(s)) result.get(keyIndex.get(s)).add(strs[i]);
             else{
-                result.add(newEntry);
-                newEntry=new ArrayList<>();
-                newEntry.add(strs[sortedStrs[i].index]);
+                temp=new ArrayList<>();
+                temp.add(strs[i]);
+                result.add(temp);
+                keyIndex.put(s,counter);
+                counter++;
             }
-            
         }
-        result.add(newEntry);
         return result;
-    }
-}
-
-class Str{
-    String s;
-    int index;
-    public Str(String s,int index){
-        this.s=new String(s);
-        this.index=index;
     }
 }
