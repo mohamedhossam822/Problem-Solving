37f9ae7029ebeee6b607b87f2927b8380aa4793b
Mohamed Hossam <33866290+mohamedhossam822@users.noreply.github.com>
2022-05-28 11:06:27 +0200

    Time: 39 ms (40.91%), Space: 59.5 MB (34.91%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -2,58 +2,40 @@ import java.util.stream.*;
 import java.util.*;
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        int n=nums.length;
+        int l,r,sum,n=nums.length,target=-1;
         List<List<Integer>> result=new ArrayList<>();
-        HashSet<List> set = new HashSet<>();
+        List<Integer> temp;
         
-        Hashtable<Integer,Integer> temptable=new Hashtable<>();
-        ArrayList<Integer> templist=new ArrayList<>();;
-        int target;
-        int diff;
-        for(int i=0;i<n;i++){
-            templist.clear();
-            templist=new ArrayList<>();
-            templist.add(nums[i]);
+        Arrays.sort(nums);
+        for(int i=0;i<n-2;i++){
+            if(nums[i]>0) break;
+            if(target==0-nums[i]) continue;
             target=0-nums[i];
-            for(int j=i+1;j<n;j++){
-                diff=target-nums[j];
-                if(temptable.containsKey(diff)){
-                    if(temptable.get(diff)!=j && temptable.get(diff)!=i){
-                        if(diff>nums[i]) {
-                            if(diff>nums[j]){
-                                if(nums[j]>nums[i]) templist.add(nums[j]);
-                                else templist.add(0,nums[j]);
-                                templist.add(diff);
-                            }
-                            else{
-                                templist.add(diff);
-                                templist.add(nums[j]);
-                            }
-                        }else{
-                            if(diff>nums[j]){
-                                templist.add(0,nums[j]);
-                                templist.add(1,diff);
-                            }else{
-                                templist.add(0,diff);
-                                if(nums[i]>nums[j]) templist.add(1,nums[j]);
-                                else templist.add(nums[j]);
-                            }
-                            
-                        } 
-                        temptable.remove(diff);
-                        set.add(templist);
-                        templist=new ArrayList<>();
-                        templist.add(nums[i]);
-                    }
+            l=i+1;
+            r=n-1;
+            while(l<r){
+               sum=nums[l]+nums[r];
+                if(sum==target){
+                    temp=new ArrayList<>();
+                    temp.add(nums[i]);
+                    temp.add(nums[l]);
+                    temp.add(nums[r]);
+                    result.add(temp);
+                    l++;
+                    while(l<n && nums[l-1]==nums[l]) l++; 
+                    r--;
+                    while(r>=0 && nums[r]==nums[r+1]) r--; 
                 }
-                else temptable.put(nums[j],j);
+                else if(sum>target){
+                    r--;
+                    while(r>=0 && nums[r]==nums[r+1]) r--; 
+                }
+                else{
+                    l++;
+                    while(l<n &&  nums[l-1]==nums[l]) l++;  
+                }
             }
         }
-        
-       for (List<Integer> x : set)
-            result.add(x);
-        
-        set.clear();
         return result;
     }
     
