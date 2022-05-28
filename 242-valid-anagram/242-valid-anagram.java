dd7541d6bfe450ef6f23e7647416c5ccd47a10df
Mohamed Hossam <33866290+mohamedhossam822@users.noreply.github.com>
2022-05-28 07:18:26 +0200

    Time: 6 ms (56.78%), Space: 43.2 MB (72.73%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,26 +1,18 @@
-import java.util.Hashtable;
+
 class Solution {
     public boolean isAnagram(String s, String t) {
-        Hashtable<Character,Integer> sFrequency=new Hashtable<>();
-        char curChar;
-        int frequency;
-        for(int i=0;i<s.length();i++){
-            curChar=s.charAt(i);
-            frequency=1;
-            if(sFrequency.containsKey(curChar)){
-                frequency=1+sFrequency.get(curChar);
-            }
-            sFrequency.put(curChar,frequency);
+        if(t.length()!=s.length()) return false;
+        int[] chars=new int[26];
+        int i;
+        for(i=0;i<s.length();i++){
+            chars[s.charAt(i)-97]++;
         }
         
-        for(int i=0;i<t.length();i++){
-            curChar=t.charAt(i);
-            if(!sFrequency.containsKey(curChar)) return false;
-            frequency=sFrequency.get(curChar);
-            if(frequency==1) sFrequency.remove(curChar);
-            else sFrequency.put(curChar,frequency-1);
+        for(i=0;i<t.length();i++){
+            chars[t.charAt(i)-97]--;
         }
-        if(sFrequency.size()!=0) return false;
+        for(i=0;i<26;i++)
+            if(chars[i]!=0) return false;
         return true;
     }
 }
