bf900fdef601d792f6378978ac98e9b7783c6a2c
Mohamed Hossam <33866290+mohamedhossam822@users.noreply.github.com>
2022-05-29 00:35:36 +0200

    Time: 15 ms (63.94%), Space: 51.2 MB (19.06%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.java
+++ 76-minimum-window-substring/76-minimum-window-substring.java
@@ -1,6 +1,7 @@
 import java.util.Queue;
 class Solution {
     public String minWindow(String s, String t) {
+        int n=t.length();
         int[] Tchars=new int[58];
         char c;
         int min=100001,start=0,end=0;
@@ -8,7 +9,6 @@ class Solution {
             c=t.charAt(i);
             Tchars[c-'A']++;
         }
-        int f=0;
         Queue<Integer> index= new LinkedList<>();
         Queue<Character> cIndex= new LinkedList<>();
         int[] chars=new int[58];
@@ -18,21 +18,14 @@ class Solution {
                 chars[c-'A']++;
                 index.add(i);
                 cIndex.add(c);
+                if(n!=0) if(Tchars[c-'A']>=chars[c-'A']) n--;
                 while(chars[c-'A']>Tchars[c-'A'] && cIndex.peek()==c){
                     chars[c-'A']--;
                     cIndex.poll();
                     index.poll();
                     c=cIndex.peek();
                 }
-                if(f==0){
-                    f=1;
-                    for(int j=0;j<58;j++){
-                        if(Tchars[j]>chars[j]){
-                            f=0;
-                            break;
-                    }}  
-                }
-                if(f==1 && (i+1-index.peek())<min){
+                if(n==0 && (i+1-index.peek())<min){
                     min= i+1-index.peek();
                     start=index.peek();
                     end=i+1;
@@ -40,7 +33,7 @@ class Solution {
                 
             }
         }
-        if(f==0) return "";
+        if(n!=0) return "";
         return s.substring(start,end);
         
     }
