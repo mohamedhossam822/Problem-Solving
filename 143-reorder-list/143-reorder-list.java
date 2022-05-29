3fc1dfbd59968b94fd4a0b7931dd901fce348c54
Mohamed Hossam <33866290+mohamedhossam822@users.noreply.github.com>
2022-05-29 15:27:30 +0200

    Time: 4 ms (25.06%), Space: 45.3 MB (79.82%) - LeetHub

--- 143-reorder-list/143-reorder-list.java
+++ 143-reorder-list/143-reorder-list.java
@@ -11,30 +11,26 @@
 class Solution {
     public void reorderList(ListNode head) {
         if(head.next==null) return;
-        Stack<ListNode> stack= new Stack<>();
         ListNode slow=head;
         ListNode fast=head.next;
-        while(fast!=null && fast.next!=null && fast.next.next!=null){
+        Stack<ListNode> s=new Stack<>();
+        while(fast.next!=null && fast.next.next!=null){
             slow=slow.next;
             fast=fast.next.next;
         }
-        
         while(slow.next!=null){
-            stack.push(slow.next);
+            s.push(slow.next);
             slow=slow.next;
-        }
-        
+        } 
         ListNode current=head;
         ListNode temp;
-        
-        while(!stack.empty()){
-            temp= current.next==null ? null: current.next;
-            current.next=stack.pop();
+        while(!s.empty()){
+            temp=current.next;
+            current.next=s.pop();
             current.next.next=temp;
             current=temp;
         }
+        current.next.next=null;
         
-        current=current.next;
-        current.next=null;
     }
 }
