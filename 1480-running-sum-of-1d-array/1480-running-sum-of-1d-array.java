7da0678dc0f018955901d8cc90873feef40781b9
Mohamed Hossam <33866290+mohamedhossam822@users.noreply.github.com>
2022-06-03 02:25:14 +0200

    Time: 0 ms (100.00%), Space: 42.3 MB (86.53%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -1,6 +1,5 @@
 class Solution {
     public int[] runningSum(int[] nums) {
-        if(nums.length==0) return nums;
         //For each element add to it the pre nums[index];
         for(int i=1;i<nums.length;i++){
             nums[i]+=nums[i-1];
