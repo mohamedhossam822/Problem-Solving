1f5fe127c93f2d0e9b05cb0af5edcc6b0f0210e8
Mohamed Hossam <33866290+mohamedhossam822@users.noreply.github.com>
2022-06-03 07:07:49 +0200

    Time: 2 ms (79.58%), Space: 51.7 MB (46.63%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.java
+++ 130-surrounded-regions/130-surrounded-regions.java
@@ -5,23 +5,16 @@ class Solution {
     public void solve(char[][] board) {
         m = board.length; //len of rows
         n = board[0].length; //len of cols
-        dir = new int[][] { { -1, 0 }, { 0, -1 }, { 1, 0 }, { 0, 1 } };
         
         //Travers the outer rows and cols to look for an entrance
         for (int j = 0; j < n; j++) {
             //For each openning we populate with 'V'
             if (board[0][j] == 'O') dfs(0 ,j, board);
-        }
-        for (int j = 0; j < n; j++) {
-            //For each openning we populate with 'V'
             if (board[m-1][j] == 'O') dfs(m-1, j, board);
         }
         for (int i = 1; i < m - 1; i++) {
             //For each openning we populate with 'V'
             if (board[i][0] == 'O') dfs(i, 0 ,board);
-        }
-        for (int i = 1; i < m - 1; i++) {
-            //For each openning we populate with 'V'
             if (board[i][n-1] == 'O') dfs(i,n-1,board);
         }
         
@@ -33,15 +26,12 @@ class Solution {
     }
 
     public void dfs(int i, int j, char[][] board) {
-        if (board[i][j] == 'V') return;
+        if (i < 0 || j < 0 || i >= m || j >= n || board[i][j] != 'O') return;
         board[i][j] = 'V';
-        int x, y;
-        //for each direction : visit if open
-        for (int[] r : dir) {
-            x = r[0] + i;
-            y = r[1] + j;
-            if (isValid(x, y) && board[x][y] == 'O') dfs(x, y, board);
-        }
+        dfs(i, j-1, board);
+        dfs(i, j+1, board);
+        dfs(i-1, j, board);
+        dfs(i+1, j, board);
     }
 
     public boolean isValid(int i, int j) {
