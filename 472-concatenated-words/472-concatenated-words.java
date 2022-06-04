47cb29708247ee76be1e3ea73fa4005155d97906
Mohamed Hossam <33866290+mohamedhossam822@users.noreply.github.com>
2022-06-04 22:29:46 +0200

    Time: 53 ms (93.80%), Space: 70.1 MB (68.71%) - LeetHub

--- 472-concatenated-words/472-concatenated-words.java
+++ 472-concatenated-words/472-concatenated-words.java
@@ -4,28 +4,32 @@ class Solution {
         List<String> res=new LinkedList<>();
         String[] subStrings;
         String toCheck;
+        int min = Integer.MAX_VALUE;
         //Add all strings to hashset
-        for(int i=0;i<words.length;i++)
+        for(int i=0;i<words.length;i++){
+            if(words[i].length()<min) min=words[i].length();
             Dict.add(words[i]);
+        }
+        if(min==0) min=1;
         //Travers through each string
         for(int i=0;i<words.length;i++){
-            if(canForm(words[i],Dict)) res.add(words[i]);
+            if(canForm(min,words[i],Dict)) res.add(words[i]);
         }
         return res;
             
     }
     
-    public boolean canForm(String s,HashSet<String> Dict){
+    public boolean canForm(int min,String s,HashSet<String> Dict){
         String left,right;
-        for(int j=0;j<s.length()-1;j++)
+        for(int j=min;j<=s.length()-min;j++)
         {
-            left=s.substring(0,j+1);
-            right=s.substring(j+1,s.length());
+            left=s.substring(0,j);
+            right=s.substring(j,s.length());
             //Each sub word is either in the dict or can be divided into further sub arrays
             //The reason we dnt check for canform(left) is bec 
             //by the end of the loop we will already have checked every possible substring
             if(Dict.contains(left))
-                if(Dict.contains(right) || canForm(right,Dict)) 
+                if(Dict.contains(right) || canForm(min,right,Dict)) 
                     return true;
         }
         return false;
