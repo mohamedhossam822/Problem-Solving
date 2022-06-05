19ea34c5ea0a075eab2b1eff4e9920fed1757a26
Mohamed Hossam <33866290+mohamedhossam822@users.noreply.github.com>
2022-06-05 12:12:45 +0200

    Time: 8 ms (36.72%), Space: 48.3 MB (47.18%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.java
+++ 47-permutations-ii/47-permutations-ii.java
@@ -1,25 +1,27 @@
+import java.util.Map.Entry;
 class Solution {
     List<List<Integer>> res = new LinkedList<>();
     
     public List<List<Integer>> permuteUnique(int[] nums) {
-        HashSet<Integer> entered = new HashSet<>();
-        backtrack(nums, new ArrayList<>(), new HashSet<>());
+        HashMap<Integer,Integer> numsFreq=new HashMap<>();
+        for(int i=0;i<nums.length;i++)
+            numsFreq.put(nums[i],numsFreq.getOrDefault(nums[i],0)+1);
+        backtrack(nums, new ArrayList<>(), numsFreq);
         return res;
     }
 
-    public void backtrack(int[] nums, List<Integer> points, HashSet<Integer> entered) {
+    public void backtrack(int[] nums, List<Integer> points, HashMap<Integer,Integer> numsFreq) {
         if (points.size() == nums.length) 
             res.add(new ArrayList<>(points));
         else{
-            HashSet<Integer> computed=new HashSet<>();
-            for (int i = 0; i < nums.length; i++){
-                if(entered.contains(i) || computed.contains(nums[i])) continue;
-                computed.add(nums[i]);
-                points.add(nums[i]);
-                entered.add(i);
-                backtrack(nums, points, entered);
-                entered.remove(i);
-                points.remove(points.size() - 1);
+            for(Entry<Integer, Integer> entry: numsFreq.entrySet()) {
+                if(entry.getValue()>0){
+                    points.add(entry.getKey());
+                    numsFreq.put(entry.getKey(),numsFreq.get(entry.getKey())-1);
+                    backtrack(nums, points, numsFreq);
+                    points.remove(points.size()-1);
+                    numsFreq.put(entry.getKey(),numsFreq.get(entry.getKey())+1);
+                }
             }
         }
     }
