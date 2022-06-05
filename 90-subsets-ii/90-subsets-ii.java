d38dea36f004a568a4b1e1953e8468897d76c96b
Mohamed Hossam <33866290+mohamedhossam822@users.noreply.github.com>
2022-06-05 10:19:10 +0200

    Time: 4 ms (22.71%), Space: 43.8 MB (63.89%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.java
+++ 90-subsets-ii/90-subsets-ii.java
@@ -10,10 +10,10 @@ class Solution {
     public  void Backtrack(int i,int[] nums,int n,List<Integer> points){
         if(i==n) return;
         points.add(nums[i]);
-        //if(!res.contains(points)){
+        if(!res.contains(points)){
             res.add(new ArrayList<>(points));
             Backtrack(i+1,nums,n,points);
-        //}
+        }
         points.remove(points.size()-1); 
         Backtrack(i+1,nums,n,points);
     } 
