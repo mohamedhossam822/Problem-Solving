6f5ead382545bd56a77a1dd07aff1a5ffaebe9e3
Mohamed Hossam <33866290+mohamedhossam822@users.noreply.github.com>
2022-06-05 11:45:45 +0200

    Time: 8 ms (36.72%), Space: 50 MB (21.38%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.java
+++ 47-permutations-ii/47-permutations-ii.java
@@ -1,18 +1,20 @@
 class Solution {
-    HashSet<List<Integer>> res = new HashSet<>();
-
+    List<List<Integer>> res = new LinkedList<>();
+    
     public List<List<Integer>> permuteUnique(int[] nums) {
         HashSet<Integer> entered = new HashSet<>();
         backtrack(nums, new ArrayList<>(), new HashSet<>());
-        return new ArrayList<>(res);
+        return res;
     }
 
     public void backtrack(int[] nums, List<Integer> points, HashSet<Integer> entered) {
         if (points.size() == nums.length) 
             res.add(new ArrayList<>(points));
         else{
+            HashSet<Integer> computed=new HashSet<>();
             for (int i = 0; i < nums.length; i++){
-                if(entered.contains(i)) continue;
+                if(entered.contains(i) || computed.contains(nums[i])) continue;
+                computed.add(nums[i]);
                 points.add(nums[i]);
                 entered.add(i);
                 backtrack(nums, points, entered);
