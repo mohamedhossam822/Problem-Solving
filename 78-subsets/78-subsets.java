26cc74dfff1fa2ca61911914c2dee9c9b6af698d
Mohamed Hossam <33866290+mohamedhossam822@users.noreply.github.com>
2022-06-05 09:31:46 +0200

    Time: 1 ms (80.52%), Space: 43.5 MB (38.48%) - LeetHub

---
+++ 78-subsets/78-subsets.java
@@ -0,0 +1,18 @@
+class Solution {
+    List<List<Integer>> res=new ArrayList<>();
+    public List<List<Integer>> subsets(int[] nums) {
+        res.add(new ArrayList<Integer>());
+        Backtrack(0,nums,nums.length,new ArrayList<Integer>());
+        return res;
+    }
+    //backtrack function to get all possible outcomes
+    public  void Backtrack(int start,int[] nums,int n,List<Integer> points){
+        if(start==n) return;
+        for(int i=start;i<n;i++){
+            points.add(nums[i]);
+            res.add(new ArrayList<>(points));
+            Backtrack(i+1,nums,n,points);
+            points.remove(points.size()-1); //Int.ofval
+        } 
+    }  
+}
