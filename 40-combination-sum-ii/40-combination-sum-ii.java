21fea0af87162e62a0d44b0ca22b6e3f99009a19
Mohamed Hossam <33866290+mohamedhossam822@users.noreply.github.com>
2022-06-05 16:17:36 +0200

    Time: 8 ms (35.50%), Space: 44 MB (45.39%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.java
+++ 40-combination-sum-ii/40-combination-sum-ii.java
@@ -2,27 +2,28 @@ class Solution {
     List<List<Integer>> res=new ArrayList<>();
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
-        backtrack(0,candidates,target,0,new ArrayList<>());
+        findCombinations(0,candidates,target,0,new ArrayList<>());
         return res;
     }
-    public void backtrack(int start,int[] candidates,int target,int sum,List<Integer> points){
+    public void findCombinations(int start,int[] candidates,int target,int sum,List<Integer> points){
+        
         if(sum==target) {
             res.add(new ArrayList<>(points));
             return;
         }
+        
         if(sum>target) return;
+        
         //To not explore already visited numbers
-        HashSet<Integer> explored=new HashSet<>();
+        int prev=-1;
+        
         //Loop through the array to get the next element
         for(int i=start;i<candidates.length;i++){
-            if(explored.contains(candidates[i])) continue;
-            explored.add(candidates[i]);
-            //Add the point to the list and add the sum
+            if(prev>=0 && candidates[i]==candidates[prev]) continue;
+            prev=i;
             sum+=candidates[i];
             points.add(candidates[i]);
-            //Explore this option
-            backtrack(i+1,candidates,target,sum,points);
-            //Back track after exploring
+            findCombinations(i+1,candidates,target,sum,points);
             sum-=candidates[i];
             points.remove(points.size()-1);
         }
