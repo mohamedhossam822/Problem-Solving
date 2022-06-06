b45eaac1e4b320c8e7fca265f2ff15cb215d8fa8
Mohamed Hossam <33866290+mohamedhossam822@users.noreply.github.com>
2022-06-06 09:43:43 +0200

    Time: 1 ms (85.84%), Space: 44.2 MB (59.38%) - LeetHub

--- 337-house-robber-iii/337-house-robber-iii.java
+++ 337-house-robber-iii/337-house-robber-iii.java
@@ -14,20 +14,25 @@
  * }
  */
 class Solution {
-    List<Integer> list=new ArrayList<>();
     public int rob(TreeNode root) {
         return getMax(root).up;   
     }
     
+    //get the max upper and bottom value for each path
     private MaxPath getMax(TreeNode root){
         if(root==null) return new MaxPath(0,0);
+        
         MaxPath left=getMax(root.left);
         MaxPath right=getMax(root.right);
         
+        //New object either the max value is root + the 2 second ones or both of the first ones
         int down=left.up+right.up;
         int up=Math.max(down,root.val+left.down+right.down);
+        
         return new MaxPath(up,down);
     }
+    
+    //A class to save the up and down for each path
     private class MaxPath{
         int up;
         int down;
