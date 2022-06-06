91da22c18bdc5fa8ac727c2ca973f62b75b349a4
Mohamed Hossam <33866290+mohamedhossam822@users.noreply.github.com>
2022-06-06 10:25:47 +0200

    Time: 2 ms (88.08%), Space: 68.7 MB (17.20%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,20 +1,11 @@
 class Solution {
     public boolean canJump(int[] nums) {
         int n=nums.length;
-        boolean[] stairValid=new boolean[n];
-        stairValid[n-1]=true;
-        //Top down approach to cheack if I can reach any true stair from my point
-        //If I can hp between true points then i can reach the end
-        for(int i=n-2;i>=0;i--){
-            for(int j=1;j<=nums[i];j++)
-            {
-                if(j==n) break;
-                if(stairValid[i+j]) {
-                    stairValid[i]=true;
-                    break;
-                }
-            }
+        int maxJumps=-1;
+        for(int i=0;i<n-1;i++){
+            maxJumps=Math.max(maxJumps-1,nums[i]);
+            if(maxJumps==0) return false;
         }
-        return stairValid[0];
+        return true;
     }
 }
