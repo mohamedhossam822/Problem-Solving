0d906cb5e471ac80145a293411b756403704dad4
Mohamed Hossam <33866290+mohamedhossam822@users.noreply.github.com>
2022-06-06 11:22:42 +0200

    Time: 10 ms (98.52%), Space: 42.9 MB (70.12%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,15 +1,29 @@
 import java.util.HashSet;
 class Solution {
+    int maxL,maxR,maxLength;
     public String longestPalindrome(String s) {
-        String maxPal="";
-        int maxLength=0,maxL=0,maxR=0,len,l,r;
+        maxLength=1;
         int n=s.length();
-        for(int i=0;i<n;i++){
-            l=i;
-            r=i;
-            while(l>-1 && r<n && s.charAt(l)==s.charAt(r)){
-                len=r-l+1;
-                if(len>maxLength){
+        int mid=n/2;
+        if(n%2==0) mid--;
+        for(int i=0;i<=n/2;i++){
+            ValidateString(mid+i,s,n);
+            if(maxLength>=2*(mid-i+1)) break;
+            if(i==0) continue;
+            ValidateString(mid-i,s,n);
+            if(maxLength>=2*(mid-i+1)) break;
+        }
+        
+        return s.substring(maxL,maxR+1);
+    }
+    
+    public void ValidateString(int i,String s,int n){
+        int l=i;
+        int r=i;
+        int len=0;
+        while(l>-1 && r<n && s.charAt(l)==s.charAt(r)){
+            len=r-l+1;
+            if(len>maxLength){
                     maxLength=len;
                     maxL=l;
                     maxR=r;
@@ -30,9 +44,5 @@ class Solution {
                 r++;
                 l--;
             }
-                
-        }
-        
-        return s.substring(maxL,maxR+1);
     }
 }
