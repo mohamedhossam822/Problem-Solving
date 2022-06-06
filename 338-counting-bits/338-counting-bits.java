736ce35659c1aa9be90735c13c3665e3a98ebac4
Mohamed Hossam <33866290+mohamedhossam822@users.noreply.github.com>
2022-06-06 20:34:10 +0200

    Time: 2 ms (78.99%), Space: 48.8 MB (7.19%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -1,14 +1,9 @@
 class Solution {
     public int[] countBits(int n) {
         int[] res=new int[n+1];
+        res[0]=0;
         for(int i=0;i<=n;i++){
-            int num = i;
-            while(num != 0)
-            {
-                res[i] += num%2;
-                num = num/2;
-            }
-            
+            res[i]=res[i/2]+i%2;
         }
         return res;
     }
