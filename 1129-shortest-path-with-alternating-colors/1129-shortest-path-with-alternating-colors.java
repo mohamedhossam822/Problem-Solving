27808b94ab4b37b84c5d8010fafc1aa2beae59ca
Mohamed Hossam <33866290+mohamedhossam822@users.noreply.github.com>
2022-06-10 19:52:22 +0200

    Time: 13 ms (31.80%), Space: 47.5 MB (61.93%) - LeetHub

--- 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.java
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.java
@@ -21,6 +21,8 @@ class Solution {
         boolean[][] visited=new boolean[2][n];
         visited[0][0]=true;
         visited[1][0]=true;
+        
+        //Add 0
         Point temp=new Point(0,false,0);
         Queue<Point> q=new LinkedList<>();
         q.add(temp);
@@ -28,17 +30,29 @@ class Solution {
             int size=q.size();
             for(int i=0;i<size;i++){
                 temp=q.poll();
+                //Update the min distance 
                 if(minDistance[temp.value]==-1) minDistance[temp.value]=temp.len;
                 else minDistance[temp.value]=Math.min(minDistance[temp.value],temp.len);
+                
+                //Mark the node as visited from either red or blue
                 if(temp.EdgeColor) visited[0][temp.value]=true;
                 else visited[1][temp.value]=true;
+                
+                //Visit the other nodes from the current node
+                
+                //if its coming from red
+                //Don't explore the node if its already coming from red
                 if(temp.value==0 || temp.EdgeColor){
                     for(int j: blueDir.get(temp.value)){
+                        
                         if(visited[1][j]) continue;
                         q.add(new Point(j,false,temp.len+1));
                     }
                         
                 }
+                
+                //if its coming from blue
+                //Don't explore the node if its already coming from blue
                 if(temp.value==0 || !temp.EdgeColor){
                     for(int j: redDir.get(temp.value)){
                         if(visited[0][j]) continue;
@@ -46,6 +60,7 @@ class Solution {
                     }
                         
                 }
+                
             }
         }
         return minDistance;
@@ -54,9 +69,9 @@ class Solution {
 }
 public class Point{
     int value;
-    boolean EdgeColor;
     //red true
     //blue false
+    boolean EdgeColor;
     int len;
     public Point(int value,boolean EdgeColor,int len){
         this.value=value;
