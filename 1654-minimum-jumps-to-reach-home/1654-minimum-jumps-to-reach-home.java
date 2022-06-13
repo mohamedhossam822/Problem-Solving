3b40ee5065dc9aeab0b058014ac09a9d82c4ef2a
Mohamed Hossam <33866290+mohamedhossam822@users.noreply.github.com>
2022-06-13 11:34:32 +0200

    Time: 17 ms (86.13%), Space: 53.9 MB (50.91%) - LeetHub

--- 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.java
+++ 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.java
@@ -1,17 +1,20 @@
 class Solution {
     boolean[] visitedF=new boolean[10001];
     boolean[] visitedB=new boolean[10001];
-    int[] visitedFI=new int[10001];
-    int[] visitedBI=new int[10001];
+    int[] cacheF=new int[10001];
+    int[] cacheB=new int[10001];
+    
     public int minimumJumps(int[] forbidden, int a, int b, int x) {
+        
         for(int i=0;i<forbidden.length;i++) {
-            visitedFI[forbidden[i]]=Integer.MAX_VALUE;
-            visitedBI[forbidden[i]]=Integer.MAX_VALUE;
+            cacheF[forbidden[i]]=Integer.MAX_VALUE;
+            cacheB[forbidden[i]]=Integer.MAX_VALUE;
             visitedF[forbidden[i]]=true; 
             visitedB[forbidden[i]]=true; 
         }
         
         int res=dfs(false,0,a,b,x);
+        
         if(res==Integer.MAX_VALUE) return -1;
         return  res;
     }
@@ -19,28 +22,32 @@ class Solution {
     public int dfs(boolean back,int index,int a,int b,int x){
         
         if(index<0 || index>10000) return Integer.MAX_VALUE;
+        
         if(!back && visitedF[index]) return Integer.MAX_VALUE;
         if(back && visitedB[index]) return Integer.MAX_VALUE;
-        if(!back && visitedFI[index]>0) return visitedFI[index];
-        if(back && visitedBI[index]>0) return visitedBI[index];
+        
+        if(!back && cacheF[index]>0) return cacheF[index];
+        if(back && cacheB[index]>0) return cacheB[index];
+        
         if(index==x) return 0;
+        
         if(!back) visitedF[index]=true;
         else visitedB[index]=true;
+        
         int result=Integer.MAX_VALUE;
+        
         result=dfs(false,index+a,a,b,x);
         if(!back) result=Math.min(result,dfs(true,index-b,a,b,x));
         
         if(back){
-            if(result==Integer.MAX_VALUE) visitedBI[index]=Integer.MAX_VALUE;
-            else visitedBI[index]=result+1;
-            return visitedBI[index];
+            if(result==Integer.MAX_VALUE) cacheB[index]=Integer.MAX_VALUE;
+            else cacheB[index]=result+1;
+            return cacheB[index];
         }
         else{
-            if(result==Integer.MAX_VALUE) visitedFI[index]=Integer.MAX_VALUE;
-            else visitedFI[index]=result+1;
-            return visitedFI[index];
-        }
-        
-        
+            if(result==Integer.MAX_VALUE) cacheF[index]=Integer.MAX_VALUE;
+            else cacheF[index]=result+1;
+            return cacheF[index];
+        }  
     }
 }
