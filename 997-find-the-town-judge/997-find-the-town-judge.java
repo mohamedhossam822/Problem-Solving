93d6d83547af1c1a683865f14f32297bd65c086a
Mohamed Hossam <33866290+mohamedhossam822@users.noreply.github.com>
2022-06-14 01:45:08 +0200

    Time: 2 ms (99.68%), Space: 74.4 MB (21.10%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.java
+++ 997-find-the-town-judge/997-find-the-town-judge.java
@@ -1,12 +1,11 @@
 import java.util.HashSet; // import the HashMap class
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        int[] people=new int[n+1];
-        people[0]=-1;
+        int[] people=new int[n+1]; //Reputation
         int l=trust.length;
         for(int i=0;i<l;i++){
-            people[trust[i][0]]=-1-n;
-            people[trust[i][1]]++;
+            people[trust[i][0]]--; //Disqualify him as town judge
+            people[trust[i][1]]++; //reputation ++
         }
         for(int i=1;i<n+1;i++){
             if(people[i]==n-1)   return i;
