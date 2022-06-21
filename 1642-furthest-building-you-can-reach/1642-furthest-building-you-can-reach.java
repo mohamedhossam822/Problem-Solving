d79928c11decaf82e80a949b95a2b94f027b7e3c
Mohamed Hossam <33866290+mohamedhossam822@users.noreply.github.com>
2022-06-21 19:06:44 +0200

    Time: 28 ms (60.66%), Space: 79.6 MB (28.56%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.java
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.java
@@ -1,19 +1,20 @@
 class Solution {
     HashMap<String,Integer> cache;
     public int furthestBuilding(int[] heights, int bricks, int ladders) {
+        int n=heights.length;
         PriorityQueue<Integer> minHeap=new PriorityQueue<>(ladders+1);
         int bricksNeeded=0;
-        for(int i=1;i<heights.length;i++){
-            if(heights[i]<=heights[i-1]) continue;
-            minHeap.add(heights[i]-heights[i-1]);
+        for(int i=0;i<n-1;i++){
+            if(heights[i+1]<=heights[i]) continue;
+            minHeap.add(heights[i+1]-heights[i]);
             if(minHeap.size()>ladders){
                 int smallest=minHeap.poll();
                 bricksNeeded+=smallest;
-                if(bricksNeeded>bricks) return i-1;
+                if(bricksNeeded>bricks) return i;
             }
         }
         
-        return heights.length-1;
+        return n-1;
     }
     
 }
