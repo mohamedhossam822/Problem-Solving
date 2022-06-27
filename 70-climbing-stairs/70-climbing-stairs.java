c30eb5915207e78ac1553e5a1364ab68e81d1f3c
Mohamed Hossam <33866290+mohamedhossam822@users.noreply.github.com>
2022-06-27 15:33:34 +0200

    Time: 0 ms (100.00%), Space: 41.1 MB (33.50%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,11 +1,12 @@
 class Solution {
     public int climbStairs(int n) {
-        int dp[]=new int[n+1];
-        dp[0]=1;
-        dp[1]=1;
+        int first=1;
+        int second=1;
         for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+            int temp=second;
+            second=first+second;
+            first=temp;
         }
-        return dp[n];
+        return second;
     }
 }
