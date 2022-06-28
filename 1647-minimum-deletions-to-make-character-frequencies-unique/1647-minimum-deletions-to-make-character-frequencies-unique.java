497330e68d22f1c6cd2a1472531c0cd0e5cf2cef
Mohamed Hossam <33866290+mohamedhossam822@users.noreply.github.com>
2022-06-28 09:24:00 +0200

    Time: 24 ms (36.15%), Space: 54.6 MB (46.42%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
@@ -3,19 +3,21 @@ import java.util.Map.Entry;
 class Solution {
     public int minDeletions(String s) {
         int max=0;
-        HashMap<Character,Integer> charFreq=new HashMap<>();
+        int[] frequency = new int[26];
         //Iterate throught string
-        for(int i=0;i<s.length();i++){
+        for (int i = 0; i < s.length(); i++) {
             char curr=s.charAt(i);
-            int freq=1;
-            if(charFreq.containsKey(curr)) freq+=charFreq.get(curr);
-            if(freq>max) max=freq;
-            charFreq.put(curr,freq);
+            frequency[curr- 'a']++;
+            if(frequency[curr- 'a']>max) max=frequency[curr- 'a'];
         }
+        
         int deletions=0;
+        
         boolean[] FreqExist=new boolean[max+1];
-        for(Entry<Character,Integer> entry: charFreq.entrySet()){
-            int currVal=entry.getValue();
+        
+        for(int i=0;i<26;i++){
+            int currVal=frequency[i];
+            if(currVal==0) continue;
             while(FreqExist[currVal]){
                 currVal--;
                 deletions++;
