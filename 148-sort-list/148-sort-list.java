dda82dff827de396c60bbd25fcdb2b88d1517645
Mohamed Hossam <33866290+mohamedhossam822@users.noreply.github.com>
2022-06-29 18:22:07 +0200

    Time: 12 ms (88.10%), Space: 75.9 MB (48.29%) - LeetHub

--- 148-sort-list/148-sort-list.java
+++ 148-sort-list/148-sort-list.java
@@ -10,19 +10,53 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-        List<Integer> list=new ArrayList<>();
-        ListNode res=new ListNode(0);
-        ListNode curr=res;
-        while(head!=null){
-            list.add(head.val);
-            head=head.next;
+        if(head==null) return head;
+        if(head.next==null) return head;
+        ListNode slow=head;
+        ListNode fast=head.next;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
         }
-        Collections.sort(list);
-        int size=list.size();
-        for(int i=0;i<size;i++){
-            curr.next=new ListNode(list.get(i));  
-            curr=curr.next;
-        } 
-        return res.next;
+        fast=slow.next;
+        slow.next=null;
+        ListNode list1=sortList(head);
+        ListNode list2=sortList(fast);
+        return merge(list1,list2);
+    }
+    
+    public ListNode merge(ListNode list1,ListNode list2){
+        ListNode l1,l2;
+        if(list1.val<=list2.val) {
+            l1=list1;
+            l2=list2;
+        }
+        else {
+            l1=list2;
+            l2=list1;
+        }
+        ListNode res=l1;
+        ListNode temp =l1;
+        
+        while (l1!=null || l2!=null) {
+            if(l1==null) {
+                temp.next=l2;
+                break;
+            }
+            if(l2==null) {
+                temp.next=l1;
+                break;
+            }
+            if(l1.val<=l2.val){
+                temp=l1;
+                l1=l1.next;
+            }else{
+                temp.next=l2;
+                temp=l2;
+                l2=l1;
+                l1=temp;
+            }
+        }
+        return res;
     }
 }
