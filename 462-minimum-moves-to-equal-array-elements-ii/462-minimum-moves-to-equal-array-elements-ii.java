30c476ddd95d2536610d1e676df688b1dc1ca6f0
Mohamed Hossam <33866290+mohamedhossam822@users.noreply.github.com>
2022-06-30 09:22:50 +0200

    Time: 90 ms (9.98%), Space: 46.6 MB (6.72%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
@@ -1,14 +1,35 @@
 class Solution {
     public int minMoves2(int[] nums) {
-        int avg=0;
         int count=0;
-        int n=nums.length;
-        Arrays.sort(nums);
-        if(n%2==1) avg=nums[n/2];
-        else avg=(nums[(n/2)-1]+nums[n/2])/2;
+        int avg=quickSelect(0,nums.length-1,nums);
         
         for(int num : nums) count+=Math.abs(num-avg);
         
         return count;
+    }
+    private int quickSelect(int start,int end,int[] nums){
+        int pivot=nums[end];
+        int position=start;
+        
+        for(int i=start;i<=end;i++){
+            if(nums[i]<pivot){
+                swap(position,i,nums);
+                position++;
+            }
+        }
+        
+        if(position==nums.length/2) return pivot; 
+        
+        swap(position,end,nums);
+        
+        if(position<(nums.length/2)) return quickSelect(position+1,end,nums); 
+        
+        return quickSelect(start,position-1,nums); 
+    }
+    
+    private void swap(int i, int j,int[] nums){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
     }
 }
