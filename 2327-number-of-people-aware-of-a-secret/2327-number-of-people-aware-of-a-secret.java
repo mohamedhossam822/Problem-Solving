9f797020754c98fdf215875c6b476d01f0821237
Mohamed Hossam <33866290+mohamedhossam822@users.noreply.github.com>
2022-07-03 07:10:48 +0200

    Time: 58 ms (66.67%), Space: 39.7 MB (77.78%) - LeetHub

--- 2327-number-of-people-aware-of-a-secret/2327-number-of-people-aware-of-a-secret.java
+++ 2327-number-of-people-aware-of-a-secret/2327-number-of-people-aware-of-a-secret.java
@@ -1,21 +1,17 @@
 class Solution {
     public int peopleAwareOfSecret(int n, int delay, int forget) {
-        long[] cache=new long[n];
-        boolean[] visited=new boolean[n];
-        
-        return (int)visit(0,n, delay, forget,cache,visited);
-    }
-    private long visit(int index,int n, int delay, int forget,long[] cache,boolean[] visited){
-        if(visited[index]) return cache[index];
-        if((index+forget)>=n) cache[index]=1;
-        
-        for(int i=delay+index;i<index+forget;i++){
-            if(i>=n) break;
-            cache[index]+=visit(i,n,delay,forget,cache,visited);
-            cache[index]%=1000000007;
+        long[] dp=new long[n];
+        for(int i=n-1;i>=0;i--)
+        {
+            if((i+forget)>=n) dp[i]=1;
+            
+            int end=Math.min(i+forget,n);
+            for(int j=delay+i;j<end;j++){
+                dp[i]+=dp[j];
+                dp[i]%=1000000007;
+            }
         }
-        visited[index]=true;
-        return cache[index];
         
+        return (int)dp[0];
     }
 }
