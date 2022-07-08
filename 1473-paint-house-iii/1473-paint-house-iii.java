72a3bfc8a80bbe598c2763716b5508ba9c190234
Mohamed Hossam <33866290+mohamedhossam822@users.noreply.github.com>
2022-07-09 00:47:07 +0200

    Time: 34 ms (73.19%), Space: 42.8 MB (97.16%) - LeetHub

--- 1473-paint-house-iii/1473-paint-house-iii.java
+++ 1473-paint-house-iii/1473-paint-house-iii.java
@@ -2,40 +2,41 @@ class Solution {
     int[][][] grid;
     public int minCost(int[] houses, int[][] cost, int m, int n, int target) {
         grid=new int[m+1][n+1][target+1];
-        return backTrack(0,0,m,0,target,houses,cost,n);
+        return backTrack(0,0,m,target,houses,cost,n);
     }
     
-    public int backTrack(int index,int preColor,int m,
-                         int neigbh,int target,int[] houses, int[][] cost, int n){
+    public int backTrack(int index,int preColor,int m,int target,int[] houses, int[][] cost, int n){
         
         if(m==index){
-            if(target==neigbh) return 0;
+            if(target==0) return 0;
             return -1;
         }
-        if(neigbh>target) return -1;
-        if(grid[index][preColor][neigbh]!=0) return grid[index][preColor][neigbh];
+        if(target<0) return -1;
         
-        int min=Integer.MAX_VALUE;
+        if(grid[index][preColor][target]!=0) return grid[index][preColor][target];
+        
+        
+        grid[index][preColor][target]=Integer.MAX_VALUE;
+        int oldtarget=target;
         if(houses[index]!=0){
-            if(preColor!=houses[index]) neigbh++;
-            preColor=houses[index];
-            return backTrack(index+1,preColor,m,neigbh,target,houses,cost,n);
+            if(preColor!=houses[index]) target--;
+            grid[index][preColor][oldtarget]=backTrack(index+1,houses[index],m,target,houses,cost,n);
+            return grid[index][preColor][oldtarget];
         }
         
         for(int i=1;i<=n;i++){
             int sum=cost[index][i-1];
-            if(preColor!=i) neigbh++;
-            int val2=backTrack(index+1,i,m,neigbh,target,houses,cost,n);
+            if(preColor!=i) target--;
+            int val2=backTrack(index+1,i,m,target,houses,cost,n);
             if(val2!=-1){
                 sum+=val2;
-                if(sum<min) min=sum;
+                if(sum<grid[index][preColor][oldtarget]) grid[index][preColor][oldtarget]=sum;
             }
-            if(preColor!=i) neigbh--;
+            if(preColor!=i) target++;
             sum=0;
             
         }
-        if(min==Integer.MAX_VALUE) min=-1;
-        grid[index][preColor][neigbh]=min;
-        return min;
+        if(grid[index][preColor][oldtarget]==Integer.MAX_VALUE) grid[index][preColor][oldtarget]=-1;
+        return grid[index][preColor][oldtarget];
     }
 }
