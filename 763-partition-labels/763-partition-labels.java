1c6bca0bd6288fa95d56004017909cb998e17aad
Mohamed Hossam <33866290+mohamedhossam822@users.noreply.github.com>
2022-07-12 20:53:43 +0200

    Time: 7 ms (55.48%), Space: 42.5 MB (66.78%) - LeetHub

--- 763-partition-labels/763-partition-labels.java
+++ 763-partition-labels/763-partition-labels.java
@@ -1,37 +1,37 @@
 class Solution {
     public List<Integer> partitionLabels(String s) {
-        List<Integer> result=new ArrayList<>();
-        LinkedHashMap<Character, int[]> lhm= new LinkedHashMap<>();
-        char ch;
-        
+        LinkedHashMap<Character,charIndcies> charIndciesMap=new LinkedHashMap<>();
+        List<Integer> res=new ArrayList<>();
         for(int i=0;i<s.length();i++){
-            ch=s.charAt(i);
-            if(!lhm.containsKey(ch)){
-                lhm.put(ch,new int[]{i,i});
-            }
-            else{
-                lhm.put(ch,new int[]{lhm.get(ch)[0],i});
-            }
-        }   
-        
-        Iterator<int[]> values=lhm.values().iterator();
-        int start=0;
-        int lastBiggest = values.next()[1];
-        int[] arr;
-        
-        while (values.hasNext()){
-            arr=values.next();
-            if(lastBiggest>arr[0]){
-                lastBiggest= lastBiggest>arr[1] ? lastBiggest : arr[1];
-            }
-            else{
-                result.add(arr[0]-start);
-                start=arr[0];
-                lastBiggest=arr[1];
-            }
+            char curr=s.charAt(i);
+            if(charIndciesMap.containsKey(curr)){
+                charIndcies ch=charIndciesMap.get(curr);
+                ch.end=i;
+            }else{
+                charIndcies ch=new charIndcies();
+                ch.start=i;
+                ch.end=i;
+                charIndciesMap.put(curr,ch);
+            } 
         }
-        result.add(s.length()-start);
-        
-        return result;
+        int end=0;
+        int start=0;
+        for(charIndcies ch : charIndciesMap.values()){
+            if(end<ch.start){
+                res.add(1+end-start);
+                start=ch.start;
+                end=ch.end;
+            } 
+            else{
+                end=Math.max(end,ch.end);
+            } 
+        }
+        res.add(1+end-start);
+        return res;
+    }
+    
+    class charIndcies{
+        int start;
+        int end;
     }
 }
