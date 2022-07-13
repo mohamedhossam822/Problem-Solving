f0b94949c13d8534158e2b3f431362524c2e55f9
Mohamed Hossam <33866290+mohamedhossam822@users.noreply.github.com>
2022-07-13 20:25:04 +0200

    Time: 3 ms (92.73%), Space: 42.9 MB (71.15%) - LeetHub

--- 139-word-break/139-word-break.java
+++ 139-word-break/139-word-break.java
@@ -1,29 +1,22 @@
 class Solution {
-    boolean[] cache;
-    boolean[] cacheCh;
+    Boolean[] cache;
     public boolean wordBreak(String s, List<String> wordDict) {
         int n=s.length();
-        cache=new boolean[n];
-        cacheCh=new boolean[n];
+        cache=new Boolean[n];
         return findWord(s,n,0,wordDict);
     }
-    private boolean findWord(String s,int n,int index, List<String> wordDict){
+    private Boolean findWord(String s,int n,int index, List<String> wordDict){
         if(index==n) return true;
-        if(cache[index]) return cacheCh[index];
+        if(cache[index]!=null) return cache[index];
         int temp=0;
-        boolean val;
+        cache[index]=false; 
         for(int i=0;i<wordDict.size();i++){
             temp=wordDict.get(i).length();
             if(temp+index>n) continue;
             if(s.substring(index,temp+index).equals(wordDict.get(i))){
-                val=findWord(s,n,temp+index,wordDict);
-                cache[index]=true;
-                cacheCh[index]=val;
-                if(val) return true;
+                cache[index]=cache[index] || findWord(s,n,temp+index,wordDict);
             }   
         }
-        cache[index]=true;
-        cacheCh[index]=false;
-        return false;
+        return cache[index];
     }
 }
