85b802b7e3ba0cc229d6bacef9d30f1798a5bc45
Mohamed Hossam <33866290+mohamedhossam822@users.noreply.github.com>
2022-10-24 07:03:34 +0200

    Time: 0 ms (100.00%), Space: 40.5 MB (74.44%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int fib(int n) {
+        if(n==0) return 0;
+        if(n==1) return 1;
+        int first=0;
+        int second=1;
+        int temp;
+        for(int i=2;i<=n;i++){
+            temp=first+second;
+            first=second;
+            second=temp;
+        }
+        return second;
+    }
+}
